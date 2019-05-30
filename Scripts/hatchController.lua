bf1bb7552644b74dae342782b37e40a37f5500d0
James Austin <james@jaustin.net>
2019-05-30 22:37:45 +1200

    added the code this time, I hope.

---
+++ Scripts/hatchController.lua
@@ -0,0 +1,39 @@
+-- Declare Constants
+
+--1 = open or opening. 0 = closed or closing
+State = 0
+
+LockChannel = 1
+HingeChannel = 2
+HatchOpen = 1
+HatchClosed = 0
+Locked = 1
+Unlocked = 0
+
+-- Tick function that will be executed every logic tick
+function onTick()
+
+	--what are we supposed to be doing again? Set the current state depending on which buttons are pressed.
+	--If both buttons are pressed, or neither, then leave it in the current state. Otherwise change it.
+	open = input.getNumber(1)
+	close = input.getNumber(2)
+	
+	if open != close then
+		if open == 1 then
+			State = 1
+		else if close == 1 then
+			State = 0
+		end
+	end
+
+	-- If State is 0, then close the hatch
+	if State = 0 then
+		output.setNumber(HingeChannel, HatchClosed)
+		output.setNumber(LockChannel, Locked)
+
+	else
+		-- we're opening so unlock and open
+		output.setNumber(LockChannel, Unlocked)
+		output.setNumber(HingeChannel, HatchOpen)
+	end
+end
