296ba05bbbecf0a2c5ff5886cf3322f56c8f6f3b
James Austin <james@jaustin.net>
2019-05-31 15:10:41 +1200

    Reorganised folder structure. Updated hatch controller to use a single button Added a picture of the microcontroller layout

--- Scripts/HatchController/hatchController.lua
+++ Scripts/HatchController/hatchController.lua
@@ -1,39 +1,46 @@
--- Declare Constants
+--- Declare Constants
 
 --1 = open or opening. 0 = closed or closing
 State = 0
+--State variable to track if the button was already pressed
+ButtonPressed = false
 
 LockChannel = 1
 HingeChannel = 2
 HatchOpen = 1
 HatchClosed = 0
-Locked = 1
-Unlocked = 0
+Locked = true
+Unlocked = false
 
 -- Tick function that will be executed every logic tick
 function onTick()
 
 	--what are we supposed to be doing again? Set the current state depending on which buttons are pressed.
 	--If both buttons are pressed, or neither, then leave it in the current state. Otherwise change it.
-	open = input.getNumber(1)
-	close = input.getNumber(2)
+	button = input.getBool(1)
 	
-	if open ~= close then
-		if open == 1 then
-			State = 1
-		else if close == 1 then
-			State = 0
+	if button then
+		if not ButtonPressed then
+			
+			ButtonPressed = true
+			
+			if State == 0 then
+				State = 1
+			elseif State == 1 then
+				State = 0
+			end
 		end
+	else
+		ButtonPressed = false
 	end
 
 	-- If State is 0, then close the hatch
 	if State == 0 then
 		output.setNumber(HingeChannel, HatchClosed)
-		output.setNumber(LockChannel, Locked)
-
-	end else
+		output.setBool(LockChannel, Locked)
+	else
 		-- we're opening so unlock and open
-		output.setNumber(LockChannel, Unlocked)
 		output.setNumber(HingeChannel, HatchOpen)
+		output.setBool(LockChannel, Unlocked)
 	end
 end
