81ff0ce58b9704fa473bd28fc88a420b1b90d68f
James Austin <james@jaustin.net>
2019-06-01 16:43:45 +1200

    Reworked PID controller to be more general purpose. All variables are set from inputs in real time. Assumes that circuit logic will be used to control PID gains as required.

--- Scripts/MyPID/myPID.lua
+++ Scripts/MyPID/myPID.lua
@@ -1,64 +1,60 @@
-KpC = property.getNumber('Kp')
-KiC = property.getNumber('Ki')
-KiMaxC = property.getNumber('KiMax')
-KiMinC = property.getNumber('KiMin')
-KdC = property.getNumber('Kd')
-lower = property.getNumber('Upper Lower Bound')
-upper = property.getNumber('Upper Output Bound')
+--Declare Constants
+
+--Input channels
+SetPointChannel = 1
+ProcVarChannel = 2
+KpChannel = 3
+KiChannel = 4
+KiMaxChannel = 5
+KiMinChannel = 6
+KdChannel = 7
+
+--Output channels
+ControlOutput = 1
+POut = 2
+IOut = 3
+IboundedOut = 4
+DOut = 5
+
+-- Global Variables
 I = 0
 P0 = 0
 
 function onTick()
 	--in normal use these are the only two inputs used.
-	setPoint = input.getNumber(1)
-	procVar = input.getNumber(2)
+	setPoint = input.getNumber(SetPointChannel)
+	procVar = input.getNumber(ProcVarChannel)
 	
 	--if the channel 3 bool is true, then take the PID congfig values from the composite input in real time (good for tuning the PID controller).
 	--Otherwise we just need to adjust the gains each tick, depending on the setpoint (better stability with very wide setpoint ranges).
-	if input.getBool(3) then
-		Kp = input.getNumber(2) / setPoint
-		Ki = input.getNumber(3) / setPoint
-		Kd = input.getNumber(4) / setPoint
-		KiMinC = input.getNumber(5)
-		KiMaxC = input.getNumber(6)
-		lower = input.getNumber(7)
-		upper = input.getNumber(8)
-	else
-		Kp = KpC / setPoint
-		Ki = KiC / setPoint
-		Kd = KdC / setPoint
-	end
-	
+	Kp = input.getNumber(KpChannel)
+	Ki = input.getNumber(KiChannel)
+	Kd = input.getNumber(KdChannel)
+	KiMinC = input.getNumber(KiMaxChannel)
+	KiMaxC = input.getNumber(KiMinChannel)
+
 	error = setPoint - procVar
 	
 	P = error * Kp
 	--debug
-	output.setNumber(2, P)
+	output.setNumber(POut, P)
 	
 	I = I + error * Ki
-
 	--debug
-	output.setNumber(3, I)
+	output.setNumber(IOut, I)
 
 	--Limit I to prevent integral windup
-		I = math.min(KiMaxC,math.max(KiMinC, I))
-	
+	I = math.min(KiMaxC,math.max(KiMinC, I))
 	--debug
-	output.setNumber(4, I)
+	output.setNumber(IboundedOut, I)
 	
 	D = Kd * (P0 - procVar)
 	--To calculate D next tick
 	P0 = procVar
 	--debug
-	output.setNumber(5, D)
-	
+	output.setNumber(DOut, D)
+
 	out = P + I + D
-	--debug
-	output.setNumber(6, out)
-	
-	--Keep the output inside the bounds specified in the property fields
-	out = math.min(upper,math.max(lower, out))
-	
 	--Output result
-	output.setNumber(1, out)
+	output.setNumber(ControlOutput, out)
 end
