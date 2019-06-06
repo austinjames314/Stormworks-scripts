d6dd2de275446730ea617f1cb0dd2f4f6965d31d
James Austin <james@jaustin.net>
2019-06-06 12:26:43 +1200

    Refactored the base PID code to: A. use a PID update function, which will be good for systems that have multiple PID loops, and B. be optimised for the lua minifier - this file gets 75% compression now.
    Does it work? I don't know! No testing has been done

--- Scripts/MyPID/myPID.lua
+++ Scripts/MyPID/myPID.lua
@@ -1,58 +1,86 @@
 --Declare Constants
 
 --Input channels
-SetPointChannel = 1
-ProcVarChannel = 2
-KpChannel = 3
-KiChannel = 4
-KiMaxChannel = 5
-KiMinChannel = 6
-KdChannel = 7
+local SetPointChannel = 1
+local ProcVarChannel = 2
+local KpChannel = 3
+local KiChannel = 4
+local KiMaxChannel = 5
+local KiMinChannel = 6
+local KdChannel = 7
 
 --Output channels
-ControlOutput = 1
-POut = 2
-IOut = 3
-IboundedOut = 4
-DOut = 5
+local ControlOutputChannel = 1
+local POut = 2
+local IOut = 3
+local IboundedOut = 4
+local DOut = 5
 
--- Global Variables
-I = 0
-P0 = 0
+-- Global Variables that need intialising
+local I = 0
 
-function onTick()
-	setPoint = input.getNumber(SetPointChannel)
-	procVar = input.getNumber(ProcVarChannel)
-	
+-- Global Variables that don't need initialising, plus variables that don't need to be global that are declared here, to help the minifier
+local P0, error, processVariable, setPoint
+
+-- These system functions that get called a lot are put in these wrapper functions, so that the minifier can shrink the code used to call them.
+function getN(channelNumber)
+    return input.getNumber(channelNumber)
+end
+
+--Looks like I don't need this one for the PID code
+--[[
+function getB(channelNumber)
+    return input.getBool(channelNumber)
+end
+--]]
+
+function setN(channelNumber, value)
+    output.setNumber(channelNumber, value)
+end
+
+--Looks like I don't need this one for the PID code
+--[[
+function setB(channelNumber, value)
+    output.setBool(channelNumber, value)
+end
+--]]
+
+function PID(KpChannel, KiChannel, KdChannel, IMaxChannel, IMinChannel, setPoint, processVariable, POut, IOut, IboundedOut, DOut)
 	--The gains are pulled in each tick. External ciruit logic either uses constants, or live variables from external.
-	Kp = input.getNumber(KpChannel)
-	Ki = input.getNumber(KiChannel)
-	Kd = input.getNumber(KdChannel)
-	KiMaxC = input.getNumber(KiMaxChannel)
-	KiMinC = input.getNumber(KiMinChannel)
+	Kp = getN(KpChannel)
+	Ki = getN(KiChannel)
+	Kd = getN(KdChannel)
+	iMax = getN(IMaxChannel)
+	iMin = getN(IMinChannel)
 
-	error = setPoint - procVar
+	error = setPoint - processVariable
 	
 	P = error * Kp
 	--debug
-	output.setNumber(POut, P)
+	setN(POut, P)
 	
 	I = I + error * Ki
 	--debug
-	output.setNumber(IOut, I)
+	setN(IOut, I)
 
 	--Limit I to prevent integral windup
-	I = math.min(KiMaxC,math.max(KiMinC, I))
+	I = math.min(iMax,math.max(iMin, I))
 	--debug
-	output.setNumber(IboundedOut, I)
+	setN(IboundedOut, I)
 	
 	D = Kd * (error - P0)
 	--To calculate D next tick
 	P0 = error
 	--debug
-	output.setNumber(DOut, D)
+	setN(DOut, D)
 
-	out = P + I + D
-	--Output result
-	output.setNumber(ControlOutput, out)
+	return P + I + D
+end
+
+function onTick()
+	setPoint = getN(SetPointChannel)
+	processVariable = getN(ProcVarChannel)
+
+	--Calculate and output result
+	setN(ControlOutputChannel, PID(Kp, Ki, Kd, KiMax, KiMin, setPoint, processVariable, POut, IOut, IboundedOut, DOut))
 end
