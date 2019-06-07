873be977db9dd50490434e09267d1a2c87a29dce
James Austin <james@jaustin.net>
2019-06-07 21:51:47 +1200

    Reworked the PID code completely. Now uses a table for the PID variables, to better support multiple PID loops. Added exponential smooting to the derivative value, due to Stormworks' glitchiness. Optimised for the lua minifier

--- Scripts/MyPID/myPID.lua
+++ Scripts/MyPID/myPID.lua
@@ -5,82 +5,87 @@ local SetPointChannel = 1
 local ProcVarChannel = 2
 local KpChannel = 3
 local KiChannel = 4
-local KiMaxChannel = 5
-local KiMinChannel = 6
+local IMaxChannel = 5
+local IMinChannel = 6
 local KdChannel = 7
 
 --Output channels
 local ControlOutputChannel = 1
-local POut = 2
-local IOut = 3
-local IboundedOut = 4
-local DOut = 5
+local POutChannel = 2
+local IOutChannel = 3
+local IboundedOutChannel = 4
+local DOutChannel = 5
 
 -- Global Variables that need intialising
-local I = 0
+local PIDTable1 = {}
+local error_0 = 0
 
 -- Global Variables that don't need initialising, plus variables that don't need to be global that are declared here, to help the minifier
-local P0, error, processVariable, setPoint
+local error, processVariable, setPoint, PIDStructTable, text
 
 -- These system functions that get called a lot are put in these wrapper functions, so that the minifier can shrink the code used to call them.
 function getN(channelNumber)
     return input.getNumber(channelNumber)
 end
 
---Looks like I don't need this one for the PID code
---[[
-function getB(channelNumber)
-    return input.getBool(channelNumber)
-end
---]]
-
 function setN(channelNumber, value)
     output.setNumber(channelNumber, value)
 end
 
---Looks like I don't need this one for the PID code
---[[
-function setB(channelNumber, value)
-    output.setBool(channelNumber, value)
-end
---]]
-
-function PID(KpChannel, KiChannel, KdChannel, IMaxChannel, IMinChannel, setPoint, processVariable, POut, IOut, IboundedOut, DOut)
-	--The gains are pulled in each tick. External ciruit logic either uses constants, or live variables from external.
-	Kp = getN(KpChannel)
-	Ki = getN(KiChannel)
-	Kd = getN(KdChannel)
-	iMax = getN(IMaxChannel)
-	iMin = getN(IMinChannel)
+function PID(PIDStructTable, setPoint, processVariable)
+	--The gains are pulled in each tick. External ciruit logic either uses constants, or live variables from external inputs, to support live tuning.
+	Kp = getN(PIDStructTable[KpChannel])
+	Ki = getN(PIDStructTable[KiChannel])
+	Kd = getN(PIDStructTable[KdChannel])
+	iMax = getN(PIDStructTable[IMaxChannel])
+	iMin = getN(PIDStructTable[IMinChannel])
 
 	error = setPoint - processVariable
 	
-	P = error * Kp
+	PIDStructTable.P = error * Kp
 	--debug
-	setN(POut, P)
+	setN(POutChannel, PIDStructTable.P)
 	
-	I = I + error * Ki
+	PIDStructTable.I = PIDStructTable.I + error * Ki
 	--debug
-	setN(IOut, I)
+	setN(IOutChannel, PIDStructTable.I)
 
 	--Limit I to prevent integral windup
-	I = math.min(iMax,math.max(iMin, I))
+	PIDStructTable.I = math.min(iMax,math.max(iMin, PIDStructTable.I))
 	--debug
-	setN(IboundedOut, I)
+	setN(IboundedOutChannel, PIDStructTable.I)
 	
-	D = Kd * (error - P0)
+	error = 0.1 * error + (0.9) * error_0
+	
+	PIDStructTable.D = Kd * (error - PIDStructTable.P0)
 	--To calculate D next tick
-	P0 = error
+	PIDStructTable.P0 = error
 	--debug
-	setN(DOut, D)
+	setN(DOutChannel, PIDStructTable.D)
 
-	return P + I + D
+	PIDStructTable.out = PIDStructTable.P + PIDStructTable.I + PIDStructTable.D
 end
 
 function onTick()
+	-- On the first tick ever, initialise the PID table with the appropriate values
+	if PIDTable1[KpChannel] == nil then
+		PIDTable1[KpChannel] = KpChannel
+		PIDTable1[KiChannel] = KiChannel
+		PIDTable1[IMaxChannel] = IMaxChannel
+		PIDTable1[IMinChannel] = IMinChannel
+		PIDTable1[KdChannel] = KdChannel
+		PIDTable1[ControlOutputChannel] = ControlOutputChannel
+		PIDTable1[POutChannel] = POutChannel
+		PIDTable1[IOutChannel] = IOutChannel
+		PIDTable1[IboundedOutChannel] = IboundedOutChannel
+		PIDTable1[DOutChannel] = DOutChannel
+		PIDTable1.I = 0
+		PIDTable1.P0 = 0
+	end
 	setPoint = getN(SetPointChannel)
 	processVariable = getN(ProcVarChannel)
 
 	--Calculate and output result
-	setN(ControlOutputChannel, PID(Kp, Ki, Kd, KiMax, KiMin, setPoint, processVariable, POut, IOut, IboundedOut, DOut))
+	PID(PIDTable1, setPoint, processVariable)
+	setN(ControlOutputChannel, PIDTable1.out)
 end
