17678c801f0a93f305bde35a6549d1845c05c812
James Austin <james@jaustin.net>
2020-02-22 19:43:16 +1300

    refactored heading hold PID in line with throttle control

--- Scripts/BoatController/autoHeadingPID.lua
+++ Scripts/BoatController/autoHeadingPID.lua
@@ -1,28 +1,32 @@
 --Declare Constants
 
 --Input channels
+local HeadingResetChannel = 9
+
 local SetPointChannel = 1
 local ProcVarChannel = 2
-local KpChannel = 3
-local KiChannel = 4
-local IMaxChannel = 5
-local IMinChannel = 6
-local KdChannel = 7
-local HeadingResetChannel = 8
-
---Output channels
-local ControlOutputChannel = 1
-local POutChannel = 2
-local IOutChannel = 3
-local IboundedOutChannel = 4
-local DOutChannel = 5
+local PIDTable = {}
+PIDTable.KpC = 3
+PIDTable.KiC = 4
+PIDTable.IMxC = 5
+PIDTable.IMnC = 6
+PIDTable.KdC = 7
+PIDTable.SmthC = 8
+--output channels
+PIDTable.OutC = 1
+PIDTable.PC = 2
+PIDTable.IC = 3
+PIDTable.IbC = 4
+PIDTable.DC = 5
+--PID variables
+PIDTable.I = 0
+PIDTable.Er0 = 0
+PIDTable.Pv0 = 0
 
 -- Global Variables that need intialising
-local PIDTable1 = {}
-local error_0 = 0
 
 -- Global Variables that don't need initialising, plus variables that don't need to be global that are declared here, to help the minifier
-local error, processVariable, setPoint, PIDStructTable, heading1, heading2, text
+local error, error_s, processVariable, setPoint, PIDStructTable, iMax, iMin, smooth
 
 -- These system functions that get called a lot are put in these wrapper functions, so that the minifier can shrink the code used to call them.
 local function getN(channelNumber)
@@ -35,11 +39,12 @@ end
 
 function PID(PIDStructTable, setPoint, processVariable)
 	--The gains are pulled in each tick. External ciruit logic either uses constants, or live variables from external inputs, to support live tuning.
-	Kp = getN(PIDStructTable[KpChannel])
-	Ki = getN(PIDStructTable[KiChannel])
-	Kd = getN(PIDStructTable[KdChannel])
-	iMax = getN(PIDStructTable[IMaxChannel])
-	iMin = getN(PIDStructTable[IMinChannel])
+	Kp = getN(PIDStructTable.KpC)
+	Ki = getN(PIDStructTable.KiC)
+	Kd = getN(PIDStructTable.KdC)
+	iMax = getN(PIDStructTable.IMxC)
+	iMin = getN(PIDStructTable.IMnC)
+	smooth = getN(PIDStructTable.SmthC)
 
 	--this one is special, as if pV = 001, and setPoint = 357, error should be -4, not 356
 	error = setPoint - processVariable
@@ -51,52 +56,41 @@ function PID(PIDStructTable, setPoint, processVariable)
 	
 	PIDStructTable.P = error * Kp
 	--debug
-	setN(POutChannel, PIDStructTable.P)
+	setN(PIDStructTable.PC, PIDStructTable.P)
 	
 	PIDStructTable.I = PIDStructTable.I + error * Ki
 	--debug
-	setN(IOutChannel, PIDStructTable.I)
+	setN(PIDStructTable.IC, PIDStructTable.I)
 
 	--Limit I to prevent integral windup
 	PIDStructTable.I = math.min(iMax,math.max(iMin, PIDStructTable.I))
 	--debug
-	setN(IboundedOutChannel, PIDStructTable.I)
+	setN(PIDStructTable.IbC, PIDStructTable.I)
 	
-	error = 0.1 * error + (0.9) * error_0
+	error = PIDStructTable.Pv0 - processVariable
+	PIDStructTable.Pv0 = processVariable
+	error_s = smooth * error + (1 - smooth) * PIDStructTable.Er0
 	
-	PIDStructTable.D = Kd * (error - PIDStructTable.P0)
+	PIDStructTable.D = Kd * error_s
 	--To calculate D next tick
-	PIDStructTable.P0 = error
+	PIDStructTable.Er0 = error_s
 	--debug
-	setN(DOutChannel, PIDStructTable.D)
+	setN(PIDStructTable.DC, PIDStructTable.D)
 
 	PIDStructTable.out = PIDStructTable.P + PIDStructTable.I + PIDStructTable.D
+	--debug
+	setN(PIDStructTable.OutC, PIDStructTable.out)
 end
 
 function onTick()
-	-- On the first tick ever, initialise the PID table with the appropriate values
-	if PIDTable1[KpChannel] == nil then
-		PIDTable1[KpChannel] = KpChannel
-		PIDTable1[KiChannel] = KiChannel
-		PIDTable1[IMaxChannel] = IMaxChannel
-		PIDTable1[IMinChannel] = IMinChannel
-		PIDTable1[KdChannel] = KdChannel
-		PIDTable1[ControlOutputChannel] = ControlOutputChannel
-		PIDTable1[POutChannel] = POutChannel
-		PIDTable1[IOutChannel] = IOutChannel
-		PIDTable1[IboundedOutChannel] = IboundedOutChannel
-		PIDTable1[DOutChannel] = DOutChannel
-		PIDTable1.I = 0
-		PIDTable1.P0 = 0
-	end
-	setPoint = getN(SetPointChannel)
+	setPoint = getN(SetPointChannel) -- This is OK, as external circuit logic stores the setpoint when it's reset
 	processVariable = getN(ProcVarChannel)
 	--reset the integral value when resetting the heading
 	if input.getBool(HeadingResetChannel) then
-		PIDTable1.I = 0
+		PIDTable.I = 0
 	end
 
 	--Calculate and output result
-	PID(PIDTable1, setPoint, processVariable)
-	setN(ControlOutputChannel, PIDTable1.out)
+	PID(PIDTable, setPoint, processVariable)
+	setN(PIDTable.OutC, PIDTable.out)
 end
